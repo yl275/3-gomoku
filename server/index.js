f350f180d4c2f809561443d771d422373dbec4b1
purehyacinth <purehyacinth@outlook.com>
2025-09-04 19:16:08 +0930

    Fix Missing parameter name via replacing "*" with "/*"

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
